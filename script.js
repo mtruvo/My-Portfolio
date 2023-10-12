0a4c5747f9ef29781e66178330579adffb1b5e60
mtruvo <115444201+mtruvo@users.noreply.github.com>
2023-10-12 09:45:55 +0800

    Inserting icons

---
+++ script.js
@@ -0,0 +1,10 @@
+function hideLoader() {
+  $(".page-loader").fadeOut("slow");
+}
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
